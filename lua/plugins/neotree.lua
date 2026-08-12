8520937fc0f00fda70f9053a2e5d2d98dc42a0c6
eatablekolas <tymek2004@gmail.com>
2026-08-12 21:35:23 +0200

    Add Neotree config

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_hidden = false,
+      },
+    },
+  },
+}
