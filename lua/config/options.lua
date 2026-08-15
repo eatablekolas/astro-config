586c8f3f9bc4c0a5723a5b8ab0b86359123ef746
eatablekolas <tymek2004@gmail.com>
2026-08-16 00:31:21 +0200

    Adjust cmake-tools to use pwsh

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,4 @@
-vim.opt.shell = "powershell"
+vim.opt.shell = "pwsh.exe"
 vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
 vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
 vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
